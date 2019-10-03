2d4002096d2973791f8e5785d9c3f5aed481fa79
Jeffrey Jou <jou0430@gmail.com>
2019-10-03 07:37:22 -0400

    Update Project 1.R
    Updated code to include annotations and added some more charts.

--- Project 1.R
+++ Project 1.R
@@ -1,13 +1,14 @@
-library(readr)
-library(readxl)
-library(tidyverse)
-library(ggplot2)
-library(pander)
-library(reshape2)
-library(plyr)
+#### Be sure to have the packages below installed using install.packages() function ###
 
-listing <- read.csv("/Users/Jeffrey/Desktop/Math/DATS 6101/Project 1/listings.csv")
+require(tidyverse) # Package used for data manipulation
+require(ggplot2) # Package for data visualization
+require(pander) # Package to make 
+require(reshape2) # Additional package for data manipulation
 
+# Read in AirBnB listing dataset (might need to change file path/working directory)
+listing <- read.csv("listings.csv") 
+
+# Function to remove outliers written by Dr. Lo
 outlierKD <- function(dt, var) { 
   var_name <- eval(substitute(var),eval(dt))
   na1 <- sum(is.na(var_name))
@@ -40,61 +41,129 @@ outlierKD <- function(dt, var) {
   }
 }
 
+# Use function to remove outliers from "listing" dataset
+# Enter Y when prompted in the console
 outlierKD(listing, price)
-price_to_room_type <- ggplot(listing, aes(x = factor(room_type), y = price)) +
-  geom_boxplot() + 
-  theme_bw() +
-  labs(x = "Room Type",
-       y = "Price")
-print(price_to_room_type + ggtitle("Boxplot of Price by Room Type"))
+outlierKD(listing, number_of_reviews)
 
-listing$bins <- cut(listing$number_of_reviews, breaks = seq(0, 750, 25))
-listing <- subset(listing, number_of_reviews > 0) 
-listing <- 
-  listing %>%
-  group_by(bins) %>%
-  mutate(mean_price_bins = mean(price, na.rm=T))
-
-price_to_reviews <- ggplot(listing, aes(x = as.factor(bins), y = mean_price_bins)) + 
-  geom_point() +
-  theme_bw() +
-  labs(x = "Number of Reviews",
-       y = "Mean Price (by bin)") + 
-  theme(axis.text.x = element_text(size = 7)) 
-print(price_to_reviews + ggtitle("Mean Price (by bin) vs. Number of Reviews"))
-
+# Question 1
+# Plot boxplot of number of reviews by room type
 room_to_reviews <- ggplot(listing, aes(x = as.factor(room_type), y = number_of_reviews, color = room_type)) +
   geom_boxplot() + 
   theme_bw() + 
   labs(x = "Room Type",
        y = "Number of Reviews",
        color = "Room Type") 
-print(room_to_reviews + ggtitle("Boxplot of Number of Reviews by Room Type"))
+print(room_to_reviews + ggtitle("Boxplot of Number of Reviews by Room Type")) # Print boxplot with title
 
-neighborhood_subset <- 
-  listing %>%
-  select(id, neighbourhood) %>%
-  separate(neighbourhood, into = c("N1", "N2", "N3"), sep = ",") 
-neighborhood_subset$bins <- NULL
-neighborhood_subset <- na.omit(neighborhood_subset) 
-neighborhood_subset <- melt(neighborhood_subset, id.vars = "id")
-neighborhood_subset$variable <- NULL
-
-price <- as.data.frame(cbind(listing$id, listing$price))
-names(price) <- c("id", "price")
-neighborhood_subset <- join(neighborhood_subset, price, by = id, type = "left", match = "all")
-
-
-
-### ANOVA and Tukey Tests
-anova_room_price <- aov(price ~ as.factor(room_type), data = listing)
-pander(anova_room_price)
-
-tukey_room_review <- TukeyHSD(anova_room_price)
-tukey_room_review
-
+# ANOVA test to see if mean number of reviews is different between room types
+# Conclusion: statistically significant, reject null hypothesis
 anova_room_review <- aov(number_of_reviews ~ as.factor(room_type), data = listing)
 pander(anova_room_review)
 
+# Tukey HSD test to determine which of the room types are pairwise considered different
+# Conclusion: Private to entire home  different
+# Conclusion: Shared to entire home and shared to private room not different
+tukey_room_review <- TukeyHSD(anova_room_review)
+tukey_room_review
+
+# Question 2
+# Boxplot of price by room type
+price_to_room_type <- ggplot(listing, aes(x = factor(room_type), y = price, color = room_type)) +
+  geom_boxplot() + 
+  theme_bw() +
+  labs(x = "Room Type",
+       y = "Price",
+       color = "Room Type")
+print(price_to_room_type + ggtitle("Boxplot of Price by Room Type")) # Print boxpot with title
+
+# One-way ANOVA test to see if mean prices are different between room types
+# Conclusion: statistically significant, reject null hypothesis
+anova_room_price <- aov(price ~ as.factor(room_type), data = listing)
+pander(anova_room_price) # Print formatted table
+
+# Tukey HSD test to determine which of the room types are pairwise considered different
+# Conclusion: all different from one another 
 tukey_room_price <- TukeyHSD(anova_room_price)
 tukey_room_price
+
+# Question 3 
+# Separate neighborhoods by unit id
+neighborhood <- 
+  as.data.frame(listing) %>%
+  select(id, price, neighbourhood)
+
+neighborhood <- separate(neighborhood, neighbourhood, into = c("N1", "N2", "N3"), sep = ",")
+neighborhood <-melt(neighborhood, 
+       id.vars = c("id", "price"),
+       variable.name = "neighborhood",
+       na.rm=T) %>%
+  arrange(id) 
+names(neighborhood) <- c("id", "price", "number", "neighborhood")
+
+# Histogram of number of AirBnBs by neighborhood
+freq_table <- 
+  neighborhood %>%
+  group_by(neighborhood) %>%
+  count() %>%
+  arrange(-n)
+
+neigh_bar <- ggplot(subset(freq_table, n > quantile(freq_table$n, 0.85)), aes(x = neighborhood, y = n)) +
+  geom_bar(aes(fill=neighborhood), stat="identity") + 
+  theme_bw() + 
+  labs(x = "Neighborhood",
+       y = "Number of AirBnbs",
+       fill = "Neighborhood") + 
+  theme(axis.ticks.x = element_blank()) + 
+  scale_x_discrete(labels = c("Blmn.", 
+                              "K St.",
+                              "Kingman",
+                              "Lincoln",
+                              "Logan",
+                              "Mt. Pleasant",
+                              "Pleasant",
+                              "Stanton",
+                              "Truxton",
+                              "Hill",
+                              "Columbia Hgts",
+                              "Dupont",
+                              "Edgewood",
+                              "Shaw",
+                              "Union Station"))
+print(neigh_bar + ggtitle("Top 15 Most Popular AirBnB Neighborhoods")) # Print bar chart
+
+# Scatterplot of mean price by neighborhood
+price_table <-
+  as.factor(neighborhood$neighborhood)
+  group_by(neighborhood$neighborhood) %>%
+  mutate(mean_price = mean(price, na.rm=T))
+
+
+
+
+# Question 5
+# Bin the number of reviews by 25 review bins
+listing$bins <- cut(listing$number_of_reviews, breaks = seq(0, 750, 25))
+listing <- subset(listing, number_of_reviews > 0) 
+listing <- 
+  listing %>%
+  group_by(bins) %>%
+  mutate(mean_price_bins = mean(price, na.rm=T)) # Take mean price by review bin
+
+# Scatterplot of average price (by bin) vs. number of reviews
+price_to_reviews <- ggplot(listing, aes(x = as.factor(bins), y = mean_price_bins)) + 
+  geom_point() +
+  theme_bw() +
+  labs(x = "Number of Reviews",
+       y = "Mean Price (by bin)") + 
+  theme(axis.text.x = element_text(size = 7)) 
+print(price_to_reviews + ggtitle("Mean Price (by bin) vs. Number of Reviews")) # Print scatterplot with title
+
+# Scatterplot of price by number of reviews by room type
+price_to_reviews_to_room <- ggplot(listing, aes(x = number_of_reviews, y = price, color = room_type)) +
+  geom_point() + 
+  theme_bw() +
+  labs(x = "Number of Reviews",
+       y = "Price",
+       color = "Room Type")
+print(price_to_reviews_to_room + facet_grid(.~room_type) + ggtitle("Scatterplot of Price by Number of Reviews")) # Print scatterplot with title
